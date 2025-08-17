7436ee69b7791652308105d706c03ee6bda96b1e
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-17 16:47:00 -0500

    feat: update routing to include register path and apply redirect guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,30 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Layout } from './modules/shared/components/layout/layout';
+import { redirectGuard } from './guards/redirect-guard';
+import { authGuard } from './guards/auth-guard';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   {
-    path: 'app',
+    path: '',
     component: Layout,
+    canActivate: [redirectGuard],
     children: [
       {
         path: 'login',
         component: Login
+      },
+      {
+        path: 'register',
+        component: Register
       }
     ]
+  },
+  {
+    path: 'app',
+    component: Layout,
+    canActivate: [authGuard],
+    children: []
   }
 ];
