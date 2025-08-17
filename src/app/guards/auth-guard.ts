0ca9dcafe3c597b8aabb424b84fa67f96d4f599a
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-17 16:46:40 -0500

    feat: add authGuard and redirectGuard for route protection based on token validity

---
+++ src/app/guards/auth-guard.ts
@@ -0,0 +1,12 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Token } from '../services/token';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const isValidToken: string | unknown = inject(Token).isValidRefreshToken();
+  if (!isValidToken) {
+    inject(Router).navigate(['/login']);
+    return false;
+  }
+  return true;
+};
