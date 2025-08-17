b94b0bba9395c14b7793f3ce6c93aa69e0dbaa4a
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-17 16:46:32 -0500

    feat: update navbar to show login/register buttons based on authentication state

--- src/app/modules/shared/components/navbar/navbar.ts
+++ src/app/modules/shared/components/navbar/navbar.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Token } from '../../../../services/token';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ButtonModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class Navbar {
+  private token = inject(Token);
+  private router = inject(Router);
 
+  get isLoggedIn(): boolean {
+    return this.token.isValidToken();
+  }
+
+  get isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
 }
