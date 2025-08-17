3206117e03552834100702c559f0899f2e6c11ab
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-17 17:57:36 -0500

    feat: add landing page route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import LandingPage from './pages/landing/landing';
 import { Layout } from './modules/shared/components/layout/layout';
 import { redirectGuard } from './guards/redirect-guard';
 import { authGuard } from './guards/auth-guard';
 import { Register } from './pages/register/register';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPage,
+    pathMatch: 'full'
+  },
   {
     path: '',
     component: Layout,
