2604158ca7f0d79290b3e19bc572b306065cb3ad
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-17 23:00:10 -0500

    feat: display user's first name in dashboard greeting and implement user info retrieval

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { Auth } from '../../services/auth';
+import { UserInfo } from '../../models/auth.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,5 +12,17 @@ import { ButtonModule } from 'primeng/button';
   styleUrls: ['./dashboard.css']
 })
 export default class DashboardPage {
-  
+  private readonly authService = inject(Auth);
+  userInfo: UserInfo | null = null;
+
+  ngOnInit() {
+    this.authService.getUserInfo().subscribe({
+      next: (userInfo) => {
+        this.userInfo = userInfo;
+      },
+      error: (error) => {
+        console.error('Error fetching user info:', error);
+      }
+    });
+  }
 }
