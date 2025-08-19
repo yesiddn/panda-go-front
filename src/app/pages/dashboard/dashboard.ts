340f08f200e923c31f63b00331eedab53060af5e
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-18 21:07:38 -0500

    feat: update dashboard to display user-specific content based on role

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -6,11 +6,12 @@ import { UserInfo } from '../../models/auth.model';
 import { CollectionRequestService } from '../../services/collection-request';
 import { CollectionRequest } from '../../models/collection-requests.model';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ButtonModule, Tag, DatePipe],
+  imports: [ButtonModule, Tag, DatePipe, RouterLink],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
@@ -18,12 +19,14 @@ export default class DashboardPage {
   private readonly authService = inject(Auth);
   private readonly collectionRequestService = inject(CollectionRequestService);
   userInfo: UserInfo | null = null;
+  role: string = 'user';
   collectionRequests: CollectionRequest[] = [];
 
   ngOnInit() {
     this.authService.getUserInfo().subscribe({
       next: (userInfo) => {
         this.userInfo = userInfo;
+        this.role = userInfo.groups && userInfo.groups.length ? userInfo.groups[0] : 'user';
       },
       error: (error) => {
         console.error('Error fetching user info:', error);
